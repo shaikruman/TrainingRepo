7eb17f104213cfe9e3e84b69947fe58f6989cd62
ruman shaik <shaikruman28@gmail.com>
2023-11-02 12:45:04 -0500

    report status of bhaijan changed good -> excellent

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App(props) {
   return (
 <div >
 <div style={{display: 'flex'}}>
-<Cart mname="Bhajrangi Bhaijaan" mdirector="Kabir Khan" mreport="Good" msrc="Bhaijan.jpeg"></Cart>
+<Cart mname="Bhajrangi Bhaijaan" mdirector="Kabir Khan" mreport="Excellent" msrc="Bhaijan.jpeg"></Cart>
     <Cart mname="Jodha Akbar" mdirector="Ashutosh Gowariker" mreport="Good" msrc="Jodha.jpeg"></Cart>
     <Cart mname="RRR" mdirector="Sri Salman Khan Garu" mreport="Excellent" msrc="RRR.jpeg" ></Cart>
     </div>
